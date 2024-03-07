afb3d24d513d2846b9784c92e706c02576dd351f
Arath Grarcía <arathgg143@gmail.com>
2024-03-07 01:08:18 -0600

    Update in I2C drivers: fuction to get PLCK value added

--- drivers/Src/stm32f103xx_i2c_driver.c
+++ drivers/Src/stm32f103xx_i2c_driver.c
@@ -6,7 +6,8 @@
  */
 
 #include"stm32f103xx_i2c_driver.h"
-
+uint16_t AHB_PreScaler[8] = {2,4,8,16,32,64,128,256,512};
+uint8_t APB1_PreScaler[4] = {2,4,8,16};
 
 /**************************************************************************************************
  * @fn 					- I2C_PeriClockControl
@@ -62,6 +63,49 @@ void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi){
 	}
 }
 
+uint32_t RCC_GetPLLOutputClock(){
+
+	return 0;
+}
+
+uint32_t RCC_GetPCLK1Value(void){
+
+	uint32_t pclk1, SystemClk;
+
+	uint8_t clksrc, temp, ahpb, apb1p;
+
+	clksrc = (RCC->CFGR >> 2) & 0x3;
+
+	if(clksrc == 0){
+		SystemClk = 8000000;
+	}else if(clksrc == 1){
+		SystemClk = 8000000;
+	}else if(clksrc == 2){
+		SystemClk = RCC_GetPLLOutputClock();
+	}
+
+	//ahbp
+	temp = (RCC->CFGR >> 4) & 0xF;
+
+	if(temp < 8){
+		ahbp = 1;
+	}else{
+		ahpb = AHB_PreScaler[temp - 8];
+	}
+
+	//apb1
+	temp = (RCC->CFGR >> 8) & 0x7;
+
+	if(temp < 4){
+		apb1p = 1;
+	}else{
+		ahpb = APB1_PreScaler[temp - 4];
+	}
+
+	pclk1 = (SystemClk / ahpb) / apb1p;
+
+	return pclk1;
+}
 /**************************************************************************************************
  * @fn 					- I2C_Init
  *
@@ -84,15 +128,8 @@ void I2C_Init(I2C_Handle_t *pI2CHandle){
 	// First lets configure the SPI_CR1 register
 	uint32_t tempreg = 0;  // temporal register
 
-	pI2CHandle->pI2Cx->CR1 = tempreg;
-	// 1. Configure the SCLSpeed
-	if(pI2CHandle->I2C_Config.I2C_SCLSpeed == I2C_SCL_SPEED_SM){
-		tempreg |= (0 << 15);
-		pI2CHandle->pI2Cx->CCR = tempreg;
-	}else if(pI2CHandle->I2C_Config.I2C_SCLSpeed == I2C_SCL_SPEED_FM4K){
-		tempreg |= (1 << 15);
-		pI2CHandle->pI2Cx->CCR = tempreg;
-	}
+	tempreg |= pI2CHandle->I2C_Config.I2C_AckControl << 10;
+
 }
 
 
